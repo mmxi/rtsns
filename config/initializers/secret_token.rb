0c78839687b9af64f9f9665d321f6c9cc8081cc4
Steven Wang <cjdx4311@gmail.com>
2012-01-13 22:54:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtsns::Application.config.secret_token = '0548753da88cf28751425f72e46ee32db1e9d41c9838490da23945d79597bfb324c523d88b783637c66565306f2537f507042c548ab29e173501f76ffd62b4a0'
