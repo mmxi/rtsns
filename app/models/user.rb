e59a746a6e9c5ebfa6955e39c665c94397e68ebe
Steven Wang <cjdx4311@gmail.com>
2012-01-15 21:49:38 +0800

    update devise registration edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  def update_with_password(params={})
+    params.delete(:current_password)
+    self.update_without_password(params)
+  end
 end
