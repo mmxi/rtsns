583427f7179c6b9e97a25c39b2daa8f7aebeb597
Steven Wang <cjdx4311@gmail.com>
2012-01-16 14:09:14 +0800

    delete sessions controller, using devise default

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rtsns::Application.routes.draw do
 
-  devise_for :users, :controllers => { :sessions => "sessions" }
+  devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
