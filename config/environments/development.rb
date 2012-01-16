cb58c613e7a882c715cfb95385a7f768a2ef47fe
Steven Wang <cjdx4311@gmail.com>
2012-01-16 16:18:35 +0800

    change forget password view and config devise send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,16 @@ Rtsns::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.163.com",
+    :port => "25",
+    :authentication => :plain,
+    :user_name => "service_170575",
+    :password => "fuck4311,.",
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
